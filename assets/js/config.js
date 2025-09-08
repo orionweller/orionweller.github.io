033724a705fda2cdb962cfebbaff7f24883450f6
Orion Weller <wellerorion@gmail.com>
2025-09-08 12:36:24 -0400

    updated site

---
+++ assets/js/config.js
@@ -0,0 +1,17 @@
+// ---------------------------------------------------------
+// Config
+// ---------------------------------------------------------
+
+var fieldName = {
+    "answer": "Answer",
+}
+var shortName = {
+    "answer": "answer",
+}
+var longDesc = {
+    "answer": "Answer",
+}
+var shortcutKey = {
+    "answer": "a",
+}
+
